47910a2405b568c9ada29c2ebb929efde262074c
Akshar Raaj <akshar@urbanpiper.com>
2024-08-24 09:49:02 +0530

    Added a decorator to retry executing the query in case of server dropped connections.

--- database.py
+++ database.py
@@ -15,43 +15,95 @@ CREATE TABLE questions (
 )
 """
 
+TABLE_QUESTION_DROP = """
+DROP TABLE IF EXISTS questions;
+"""
+
 TYPE_DIFFICULTY_CREATE = """
 CREATE TYPE difficulty AS ENUM ('easy', 'medium', 'hard')
 """
 
+TYPE_DIFFICULTY_DROP = """
+DROP TYPE difficulty
+"""
 
-def _create_difficulty_enum():
-    connection = psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-    )
-    cursor = connection.cursor()
-
-    cursor.execute(TYPE_DIFFICULTY_CREATE)
-
-    connection.commit()
-    connection.close()
+connection = None
 
 
-def _create_table_question():
-    connection = psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASSWORD,
-    )
-    cursor = connection.cursor()
-
-    cursor.execute(TABLE_QUESTION_CREATE)
-
-    connection.commit()
-    connection.close()
+def get_database_connection(force=False):
+    global connection
+    if connection is None or force:
+        connection = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD,
+        )
+    return connection
 
 
-def create_tables():
-    _create_difficulty_enum()
-    _create_table_question()
+def retry_with_new_connection(func):
+    def wrapper(*args, **kwargs):
+        try:
+            # For idle connections, the connection might be closed by the server
+            # In such cases, executing the query will raise psycopg2.OperationError.
+            # OperationError is handled by psycopg2. But connection.closed is set True then.
+            # Running the query again will raise psycopg2.InterfaceError
+            # which we are handling here
+            return func(*args, **kwargs)
+        except psycopg2.InterfaceError as e:
+            print("handling interface error")
+            get_database_connection(force=True)
+            return func(*args, **kwargs)
+    return wrapper
+
+
+@retry_with_new_connection
+def _create_tables():
+    # Helper function to create the tables.
+    with get_database_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(TYPE_DIFFICULTY_CREATE)
+            cursor.execute(TABLE_QUESTION_CREATE)
+
+
+@retry_with_new_connection
+def _drop_tables():
+    # Helper function to drop the tables. Be extremely cautious!
+    with get_database_connection() as connection:
+        with connection.cursor() as cursor:
+            cursor.execute(TABLE_QUESTION_DROP)
+            cursor.execute(TYPE_DIFFICULTY_DROP) 
+
+
+
+@retry_with_new_connection
+def create_question(question_text: str, kanda: str | None = None, tags: list[str] | None = None, difficulty: str | None = None):
+    inserted_id = None
+    connection = get_database_connection()
+    # We have created a context.
+    # Hence transaction will be committed on successful execution of the block
+    # or else it would be rolled back
+    # No need to explicitly call connection.commit()
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "INSERT INTO questions (question, kanda, tags, difficulty) VALUES (%s, %s, %s, %s) RETURNING id",
+                (question_text, kanda, tags, difficulty),
+            )
+            inserted_id = cursor.fetchone()[0]
+    return inserted_id
+
+
+# Write a function to retrieve the questions
+@retry_with_new_connection
+def get_questions():
+    connection = get_database_connection()
+    with connection:
+        with connection.cursor() as cursor:
+            # id is the primary key, hence has an index
+            # We are ordering on an indexed field
+            cursor.execute("SELECT question, tags, difficulty, kanda FROM questions order by id desc")
+            return cursor.fetchall()
+    return []
