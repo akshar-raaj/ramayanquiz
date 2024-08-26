fd7a9344ed7051b1db67a9f076325030cc881697
Akshar Raaj <akshar@urbanpiper.com>
2024-08-26 16:57:13 +0530

    Allow CORS requrests from Frontend

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import Question
 from database import create_question
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+    "http://ramayanquiz.com",
+    "https://ramayanquiz.com"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/_health")
