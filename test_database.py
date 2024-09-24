db0f6fe2cf8271bb59f63d7e1fe252a6f12fc735
Akshar Raaj <akshar@urbanpiper.com>
2024-09-24 19:36:27 +0530

    Added few test

--- test_database.py
+++ test_database.py
@@ -1,8 +1,8 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock, patch, MagicMock
 from psycopg2 import InterfaceError
 
 
-from database import get_database_connection, retry_with_new_connection
+from database import get_database_connection, retry_with_new_connection, _create_tables, _drop_tables
 from constants import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
 
 
@@ -32,6 +32,8 @@ def test_retry_with_new_connection(mocked_get_connection):
     # Simulates a mock function that uses connection to make a db query
     dummy_function = Mock()
     mocked_return_value = Mock()
+    # In the first invocation, it will raise an InterfaceError
+    # In the second invocation, it will return mocked_return_value
     dummy_function.side_effect = [InterfaceError, mocked_return_value]
     # Mimic decorating dummy function
     wrapper = retry_with_new_connection(dummy_function)
@@ -40,3 +42,26 @@ def test_retry_with_new_connection(mocked_get_connection):
     mocked_get_connection.assert_called_with(force=True)
     assert return_value == mocked_return_value
     assert dummy_function.call_count == 2
+
+
+@patch('database.get_database_connection')
+def test_create_tables(mocked_get_connection):
+    # Need MagicMock instead of Mock because connection is used as a context manager
+    # and hence need a __enter__ dunder method.
+    mocked_connection = MagicMock()
+    mocked_get_connection.return_value = mocked_connection
+    mocked_cursor = MagicMock()
+    mocked_connection.cursor.return_value = mocked_cursor
+    _create_tables()
+    assert mocked_get_connection.called
+
+@patch('database.get_database_connection')
+def test_drop_tables(mocked_get_connection):
+    # Need MagicMock instead of Mock because connection is used as a context manager
+    # and hence need a __enter__ dunder method.
+    mocked_connection = MagicMock()
+    mocked_get_connection.return_value = mocked_connection
+    mocked_cursor = MagicMock()
+    mocked_connection.cursor.return_value = mocked_cursor
+    _drop_tables()
+    assert mocked_get_connection.called
