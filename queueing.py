7dd1c46f1a7b77bc7e3666e72284267e651fc973
Akshar Raaj <akshar@urbanpiper.com>
2024-10-09 15:01:14 +0530

    Added connection handling during RabbitMQ publish

--- queueing.py
+++ queueing.py
@@ -1,16 +1,36 @@
 import pika
 import json
 
+from pika.exceptions import StreamLostError
 
+
+rabbit_connection = None
 QUEUE_NAME = 'translate-hindi'
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
-channel = connection.channel()
 
-channel.queue_declare(queue=QUEUE_NAME, durable=True)
+def get_rabbit_connection(force=False):
+    global rabbit_connection
+    if rabbit_connection is None or force:
+        rabbit_connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+    return rabbit_connection
 
 
-def publish(module_name: str, function_name: str, args: list):
+def retry_with_new_connection(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except StreamLostError:
+            print("handling closed TCP connection error")
+            get_rabbit_connection(force=True)
+            return func(*args, **kwargs)
+    return wrapper
+
+
+@retry_with_new_connection
+def publish(module_name: str, function_name: str, args: list, queue_name: str = QUEUE_NAME):
+    connection = get_rabbit_connection()
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME, durable=True)
     data = json.dumps({'module_name': module_name, 'function_name': function_name, 'args': args})
     channel.basic_publish(exchange='',
                           routing_key=QUEUE_NAME,
