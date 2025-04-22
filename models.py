3a424a7cc44bf4d5a40ef99d212cf9562c22a2f6
Akshar Raaj <raaj.akshar@gmail.com>
2025-04-22 17:05:55 +0530

    Added several models for response schema. It can handle serialization well and provide info in OpenAPI documentation

--- models.py
+++ models.py
@@ -30,8 +30,24 @@ class Kanda(Enum):
 
 
 class Answer(BaseModel):
+    """
+    Represents a single answer in the API request.
+    Used im POST /questions to represent one answer for a question
+    """
     answer: str
     is_correct: bool | None = False
+
+
+class AnswerResponse(Answer):
+    """
+    Represents a single answer in the API response.
+    Used in GET /questions endpoint to represent one answer for a question
+    """
+    id: int
+    # It's possible that the RabbitMQ task for translation hasn't executed yet, thus keep these
+    # inferred fields like answer_hindi, answer_telugu as optional
+    answer_hindi: str | None = None
+    answer_telugu: str | None = None
 
 
 class Question(BaseModel):
@@ -40,3 +56,13 @@ class Question(BaseModel):
     difficulty: Difficulty | None = None
     tags: list[str] | None = list()
     answers: list[Answer] = list()
+
+
+class QuestionResponse(Question):
+    id: int
+    # It's possible that the RabbitMQ task for translation hasn't executed yet, thus keep these
+    # inferred fields like question_hindi, question_telugu as optional
+    question_hindi: str | None = None
+    question_telugu: str | None = None
+    information: str | None = None
+    answers: list[AnswerResponse]
